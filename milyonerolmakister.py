de57e68b33e119eeae86e43804dc6c2fcae26469
Tarık <70340756+tarikyildiz511@users.noreply.github.com>
2020-12-15 17:46:24 +0300

    Add files via upload

---
+++ milyonerolmakister.py
@@ -0,0 +1,114 @@
+soru1 = print("Aşağıdakilerden hangisi atatürk ün doğduğu yıl dır? a(1938)b(1891)c(1882)d(1881)")
+print("Prosedür gereği şıklar sayı olarak işlenmektedir a(1) b(2) c(3) d(4)tür iyi oyunlar :))) ")
+while True:
+    cevap =int(input("Cevabı giriniz..."))
+    if cevap ==4:
+        print("doğru cevap ödülünüz 1000tl")
+        break
+    else:
+        break
+soru2 = print("bir yarışta 2.ciyi geçen kaçıncı olur?")
+while True:
+    cevap =int(input("Cevabı giriniz..."))
+    if cevap ==2:
+        print("doğru cevap ödülünüz 2000 tl ")
+        break
+    else:
+            print("yanlış cevap ödülünüz 0 tl")
+            break
+soru3 = print("Atın küçüğüne ne ad verilir?")
+while True:
+    cevap =input("Cevabı giriniz....")
+    if cevap=="tay"or"Tay"or"TAY":
+        print("Doğru cevap ödülünüz 3000 tl ")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0tl")
+        break
+soru4 = print("6kere6 36 ise ne olmuştur?")
+while True:
+    cevap = int(input("Cevabı giriniz... : Şıklar a= dedemin bıyığı b = 36 c=29 d=41"))
+    if cevap ==1:
+        print("doğru cevap ödülünüz 5000 tl")
+        break
+    else:
+        print("yanlış cevap ödülünüz0 tl")
+        break
+soru5 = print("Açılış törenlerine veya düğünlere katılamayan kişiler genellikle ne gönderirler?")
+while True:
+    cevap = int(input("cevabı giriniz... :şıklar a=çelenk b= çiçek c= çikolata d=çorap"))
+    if cevap ==1:
+        print("doğru cevap ödülünüz 10000tl barajı geçtiniz ve bir joker kazandınız joker soruyu pas etmeye yarıyor")
+        print("ARTIK ÇEKİLMEK SERBEST BARAJI GEÇTİNİZ")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0 tl ")
+        break
+print("ARTIK ÇEKİLMEK SERBEST BARAJI GEÇTİNİZ")
+soru6 = print("Şuraya ne yazıldıysa o olur diyen biri parmağıyla nereyi işaret eder")
+while True:
+    cevap = int(input("cevabı giriniz... : şıklar a=(alın) b=(ayak) c=(göz) d=(kulak)"))
+    if cevap ==1:
+        print("doğru cevap ödülünüz 25000tl ")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0 tl")
+
+soru7=print("Bir büyükelçinin temsilci olarak bulunduğu ülke dışına çıkması durumunda veya o ülkeye gelmesinden önce ona vekalet eden diplomata ne ad verilir?")
+while True:
+    cevap =int(input("cevabı giriniz... : şıklar a=(ataşe) b=(konsolos) c=(maslahatgüzar)d=(konsolos) çekilmek için 0 "))
+    if cevap==3:
+        print("doğru cevap ödülünüz 50000tl")
+        break
+    elif cevap==0:
+        print("yarışmadan çekilmeniz onaylandı ödülünüz 25000tl iyi günlerde kullanın")
+    else:
+        print("yanlış cevap ödülünz 0 tl")
+
+soru8=print("Sabah zumba dersim var akşam da spinning dersine gireceğim diyen biri bu dersleri muhtemelen nerede alacaktır?")
+while True:
+    cevap=int(input("cevabı giriniz...: şıklar a=(spor salonu) b = (düğün salonu) c=(kebapçı) d= (plaj) çekilmek için 0 yazınız" ))
+    if cevap ==1:
+        print("doğru cevap ödülünüz 100000tl")
+        break
+    elif cevap==0:
+        print("yarışmadan çekilmeniz onaylandı ödülünüz 50000tl iyi günlerde kullanın")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0 tl")
+
+soru9 = print("Kıpırdamayın, çekiyorum, çekiyorum, çektim diyen kişi muhtemelen kimdir?")
+while True:
+    cevap=int(input("cevabı giriniz... : şıklar a=(kameraman)b=(çiçekci)c=(simitçi)d=(dişçi)çekilmek için 0 tuşuna basınız"))
+    if cevap ==1:
+        print("doğru cevap ödülünüz 250000tl")
+        break
+    elif cevap ==0:
+        print("yarışmadan çekilmeniz onaylandı ödülünüz 100000tl iyi günlerde kullanın")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0tl")
+        break
+soru10= print("Akçaabat, İnegöl, Tire gibi yerlerin hangi tür yiyeceği meşhurdur?")
+while True:
+    cevap =int(input("cevabı giriniz... :şıklar a=(hamburger) b=(pizza) c=(tatlı)d=(köfte)çekilmek için 0 tuşuna basınız"))
+    if cevap ==4:
+        print("doğru cevap ödülünüz 500000tl")
+        break
+    elif cevap==0:
+        print("yarışmadan çekilmeniz onaylandı ödülünüz 250000tl iyi günlerde kullanın")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0 tl")
+        break
+soru11= print("1941'de kocası Almanlar tarafından öldürülen Mariya Oktyabrskaya, her şeyini satıp Savaşan Kız Arkadaş adını verdiği hangi aracı alarak savaşa katılmıştır?")
+while True:
+    cevap = int(input("cevabı giriniz... : şıklar a =(uçak) b=(araba) c=(kaykay) d=(tank)"))
+    if cevap ==4:
+        print("doğru cevap ödülünüz 1milyon tl tebrik ederiz iyi günlerde kullanın ")
+        break
+    elif cevap ==0:
+        print("yarışmadan çekilmeniz onaylandı ödülünüz 500000tl iyi günlerde kullanın")
+        break
+    else:
+        print("yanlış cevap ödülünüz 0 tl")
